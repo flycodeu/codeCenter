6255e3bc48d59ee5d6f94976a3de49e9b4e7fec9
flycodeu <1785613048@qq.com>
2025-04-25 15:14:15 +0800

    feat: 数组

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -40,14 +40,15 @@ export default defineUserConfig({
          * @see https://theme-plume.vuejs.press/config/basic/#blog
          */
         // blog: false, // 禁用博客
-        // blog: {
-        //   postList: true, // 是否启用文章列表页
-        //   tags: true, // 是否启用标签页
-        //   archives: true, // 是否启用归档页
-        //   categories: true, // 是否启用分类页
-        //   postCover: 'right', // 文章封面位置
-        //   pagination: 15, // 每页显示文章数量
-        // },
+        blog: {
+            postList: true, // 是否启用文章列表页
+            tags: true, // 是否启用标签页
+            archives: true, // 是否启用归档页
+            categories: true, // 是否启用分类页
+            postCover: 'right', // 文章封面位置
+            pagination: 15, // 每页显示文章数量
+            include: ['preview/*.md','algorithm/*.md'],
+        },
 
         /* 博客文章页面链接前缀 */
         article: '/article/',
@@ -63,9 +64,9 @@ export default defineUserConfig({
          * @see https://theme-plume.vuejs.press/config/basic/#autofrontmatter
          */
         autoFrontmatter: {
-          permalink: true,  // 是否生成永久链接
-          createTime: true, // 是否生成创建时间
-          title: true,      // 是否生成标题
+            permalink: true,  // 是否生成永久链接
+            createTime: true, // 是否生成创建时间
+            title: true,      // 是否生成标题
         },
 
         plugins: {
